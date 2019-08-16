d52416042e0ceab0736c2126847bcca5d609092d
smalldazzy <oleg.nod@gmail.com>
2019-08-16 13:54:39 +0300

    carousel,dynamic datalist

--- src/webparts/afisha/components/Afisha.tsx
+++ src/webparts/afisha/components/Afisha.tsx
@@ -16,7 +16,8 @@ export default class Afisha extends React.Component<IAfishaProps, {}> {
     columns: this.columsCreate(['Title', 'EventDate', 'EndDate']),
     recitems: [],
     events: [],
-    userName: string
+    userName: '', 
+    currentdate: new Date()
   };
   // private getList(listid: string) {
   //   let wep = new Web('https://cupcuper.sharepoint.com/sites/dev1');
@@ -28,6 +29,7 @@ export default class Afisha extends React.Component<IAfishaProps, {}> {
   // }
   private getSearch() {
     let url = `https://cupcuper.sharepoint.com/search/_api/search/query?querytext='a5558fa4-8101-4bd8-b487-d9cadfef789d'&selectproperties='Title%2cEventDateOWSDate%2cEndDateOWSDate'&clienttype='ContentSearchRegular'`;
+    // let url = `https://cupcuper.sharepoint.com/search/_api/search/query?querytext='5f0e9e0b-a4b4-457f-91fb-46a98119727e'&selectproperties='Title%2cEventDateOWSDate%2cEndDateOWSDate'&clienttype='ContentSearchRegular'`;
     fetch(url, {
       method: 'get',
       headers: {
@@ -35,11 +37,9 @@ export default class Afisha extends React.Component<IAfishaProps, {}> {
         'content-type': "application/json;odata=verbose",
       }
     }).then((response) => response.json()).then((d) => {
-      console.log(d.d.query.PrimaryQueryResult.RelevantResults.Table.Rows.results.slice(1));
       let arr = d.d.query.PrimaryQueryResult.RelevantResults.Table.Rows.results.slice(1);
       let viewlist = [];
       arr.forEach((item) => {
-        console.log(item.Cells.results[2].Value);
         viewlist.push({
           Title: item.Cells.results[2].Value,
           EventDate: item.Cells.results[3].Value,
@@ -48,19 +48,20 @@ export default class Afisha extends React.Component<IAfishaProps, {}> {
       });
       console.log(viewlist);
       this.setState({ recitems: viewlist });
-
-      // this.getList(d.d.query.PrimaryQueryResult.RelevantResults.Table.Rows.results[0].Cells.results[2].Value);
     }
     );
   }
   private kinopoisk() {
     let date = new Date().toISOString().split('T')[0];
+    console.log('jepa');
+    console.log(this.state.currentdate.toISOString().split('T')[0]);
+    let newDate = this.state.currentdate.toISOString().split('T')[0];
     // let dateend1 = date1;
     // dateend1.setDate(date1.getDate()+1);
     // let date = date1.toISOString().split('T')[0];
     // let dateend = dateend1.toISOString().split('T')[0];
-    console.log(date);
-    let uri = `https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=${date}&primary_release_date.lte=${date}`;
+    // console.log(date);
+    let uri = `https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=${newDate}&primary_release_date.lte=${newDate}`;
     let url = `https://cors-anywhere.herokuapp.com/https://api.themoviedb.org/3/movie/now_playing/`;
     fetch(uri, {
       method: 'get',
@@ -72,9 +73,8 @@ export default class Afisha extends React.Component<IAfishaProps, {}> {
       }
     }).then((r) => r.json())
       .then((r) => {
-        console.log(r.results);
         let events = [];
-        r.results.forEach((item) => {
+        r.results.slice(0,3).forEach((item) => {
           events.push({
             id: item.id,
             title: item.title,
@@ -82,8 +82,18 @@ export default class Afisha extends React.Component<IAfishaProps, {}> {
             date: item.release_date,
           });
         });
+        console.log(events);        
         this.setState({ events: events });
       });
+  }
+  public addTempItem(item){
+    let newRecItems = this.state.recitems;
+    newRecItems.push({
+      Title: item.title,
+      EventDate: item.date,
+      EndDate: item.date
+    });
+    this.setState({recitems: newRecItems});
   }
   public getUserData() {
     this.props.context.msGraphClientFactory
@@ -113,7 +123,6 @@ export default class Afisha extends React.Component<IAfishaProps, {}> {
           subject: item.title,
           body: {
             "contentType": "HTML",
-            "content": "eboi"
           },
           start: {
             "dateTime": item.date,
@@ -133,10 +142,10 @@ export default class Afisha extends React.Component<IAfishaProps, {}> {
         });
     });
   }
-  private addRecord(item,userName){
+  private addRecord(item, userName) {
     console.log('rab');
     let wep = new Web('https://cupcuper.sharepoint.com/sites/dev1');
-    wep.lists.getById('a5558fa4-8101-4bd8-b487-d9cadfef789d').items.add({
+    wep.lists.getById('a5558fa4-8101-4bd8-b487-d9cadfef789d'/*'5f0e9e0b-a4b4-457f-91fb-46a98119727e'*/).items.add({
       Title: item.title,
       EndDate: item.date,
       EventDate: item.date,
@@ -161,9 +170,12 @@ export default class Afisha extends React.Component<IAfishaProps, {}> {
     this.getSearch();
     this.kinopoisk();
     this.getUserData();
+    this.kinopoisk= this.kinopoisk.bind(this);
     this.createEvent = this.createEvent.bind(this);
+    this.addTempItem = this.addTempItem.bind(this);
     // this.createEvent();
   }
+
   public render(): React.ReactElement<IAfishaProps> {
     return (
       <div className={styles.afisha} >
@@ -173,10 +185,13 @@ export default class Afisha extends React.Component<IAfishaProps, {}> {
               <span className={styles.title}>Welcome to SharePoint!</span>
               <p className={styles.description}>{escape(this.props.description)}</p>
               <Covers
-                events={this.state.events.slice(0, 3)}
+                events={this.state.events}
                 calHandler={this.createEvent}
                 recHandler={this.addRecord}
                 userName={this.state.userName}
+                currentDate={this.state.currentdate}
+                kinopoisk={this.kinopoisk}
+                addTempItem={this.addTempItem}
               />
               <DetailsList
                 items={this.state.recitems}
--- src/webparts/afisha/components/Covers.tsx
+++ src/webparts/afisha/components/Covers.tsx
@@ -3,15 +3,21 @@ import styles from './Afisha.module.scss';
 
 const Covers = (props) => (
     <div>
+        <button onClick={()=>{props.currentDate.setDate(props.currentDate.getDate()-1); props.kinopoisk()}}>-</button>
     {props.events.map((item) => 
         (
             <div style={{display: 'inline-block'}}>
                 <img src={item.imgurl} width="150" height="225"></img>
                 <p>{item.title}</p>
-                <button onClick={()=>{props.calHandler(item);props.recHandler(item,props.userName);}}>Attend</button>
+                <button onClick={()=>{
+                    /*props.calHandler(item);*/
+                    props.recHandler(item,props.userName);
+                    props.addTempItem(item);
+                    }}>Attend</button>
             </div>
         )
     )}
+        <button onClick={()=>{props.currentDate.setDate(props.currentDate.getDate()+1); props.kinopoisk()}}>+</button>
     </div>
 );
 export default Covers;
